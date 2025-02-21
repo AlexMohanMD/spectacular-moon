50a1990484ca72bdbd1fac931f10ecddee91b932
Alex Mohan Morzeria-Davis <chabgo.pwn@gmail.com>
2025-02-21 12:54:03 +0000

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,4 +1,6 @@
 import { defineStackbitConfig } from '@stackbit/types';
+import { GitContentSource } from "@stackbit/cms-git";
+
 
 export default defineStackbitConfig({
     "stackbitVersion": "~0.6.0",
