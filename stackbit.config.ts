a51c90e6fe6a98ea75cc17e5ed10a2886108440f
Alex Mohan Morzeria-Davis <chabgo.pwn@gmail.com>
2025-02-21 10:36:22 +0000

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -4,6 +4,17 @@ export default defineStackbitConfig({
     "stackbitVersion": "~0.6.0",
     "nodeVersion": "18",
     "ssgName": "nextjs",
-    "contentSources": [],
+    contentSources: [
+    new GitContentSource({
+      rootPath: __dirname,
+      contentDirs: ["content"],
+      models: [
+        {
+          name: "Page",
+          // Define the model as a page model
+          type: "page",
+          urlPath: "/{slug}",
+          filePath: "content/pages/{slug}.json",
+          fields: [{ name: "title", type: "string", required: true }]]})],
     "postInstallCommand": "npm i --no-save @stackbit/types"
 })
