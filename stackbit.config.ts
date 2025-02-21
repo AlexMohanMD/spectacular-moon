a6d4c4103a53987046726d1fee728a52931dd404
Alex Mohan Morzeria-Davis <chabgo.pwn@gmail.com>
2025-02-21 12:57:17 +0000

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -6,7 +6,7 @@ export default defineStackbitConfig({
     "stackbitVersion": "~0.6.0",
     "nodeVersion": "18",
     "ssgName": "nextjs",
-    "postInstallCommand": "npm i --no-save @stackbit/types"
+    "postInstallCommand": "npm i --no-save @stackbit/types",
 contentSources: [
     new GitContentSource({
       rootPath: __dirname,
@@ -24,34 +24,8 @@ contentSources: [
       ],
     })
   ],
-  siteMap: ({ documents, models }) => {
-    // 1. Filter all page models
-    const pageModels = models.filter((m) => m.type === "page")
-
-    return documents
-      // 2. Filter all documents which are of a page model
-      .filter((d) => pageModels.some(m => m.name === d.modelName))
-      // 3. Map each document to a SiteMapEntry
-      .map((document) => {
-        // Map the model name to its corresponding URL
-        const urlModel = (() => {
-            switch (document.modelName) {
-                case 'Page':
-                    return 'otherPage';
-                case 'Blog':
-                    return 'otherBlog';
-                default:
-                    return null;
-            }
-        })();
-
-        return {
-          stableId: document.id,
-          urlPath: `/${urlModel}/${document.id}`,
-          document,
-          isHomePage: false,
-        };
-      })
-      .filter(Boolean) as SiteMapEntry[];
-  }
+   modelExtensions: [
+    { name: "page", type: "page", urlPath: "/{slug}" },
+    { name: "post", type: "page", urlPath: "/blog/{slug}" }
+  ]
 });
